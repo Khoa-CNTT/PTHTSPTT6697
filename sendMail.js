6de8bac5d113b8495b218be32dc5f0ed94a5fbc6
Ho Sang <146820466+sangho122003@users.noreply.github.com>
2025-04-26 16:58:45 +0700

    commit sendmail

---
+++ sendMail.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+let otpStore = {}; // { mail: { otp, expiresAt } }
+// Tạo transporter cho email gửi qua Gmail
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+// Hàm gửi email (có thể dùng chung cho nhiều mục đích như đăng nhập, quên mật khẩu...)
+const sendLoginTokenMail = async (toEmail, subject, htmlContent) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: toEmail,
+    subject,
+    html: htmlContent
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email đã được gửi đi thành công!');
+  } catch (error) {
+    console.error('Lỗi khi gửi email:', error);
+  }
+};
+
+module.exports = { sendLoginTokenMail };
